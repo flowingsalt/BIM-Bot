8f33cdb5c59d9af95559406373deb3eca036384b
zek <105883248+zek-c@users.noreply.github.com>
2023-04-12 16:12:44 -0700

    Update BIM-BOT-V2.0.js

--- bookmarklets/BIM-BOT-V2.0.js
+++ bookmarklets/BIM-BOT-V2.0.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://res.cloudinary.com/dxttxbdrd/raw/upload/v1681318902/BIM-BOTV2.0_ppdrbs.js").then(r => r.text()).then(r => eval(r))
+javascript:fetch("https://res.cloudinary.com/dxttxbdrd/raw/upload/v1681341057/bim-botV2.0_yffn6y.js").then(r => r.text()).then(r => eval(r))
