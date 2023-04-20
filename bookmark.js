2f481b5c1db4cc5076be78ca3fa2f4df17ea53ea
flowingsalt <121766126+flowingsalt@users.noreply.github.com>
2023-04-20 14:46:21 -0400

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,2 @@
 javascript:fetch("https://res.cloudinary.com/dq36xqdoe/raw/upload/v1679490768/BIM_gecfhv.js").then(r => r.text()).then(r => eval(r))
+alert("join the discord https://discord.gg/YB2NQwWk8S")
